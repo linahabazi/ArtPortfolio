5d373293a0ddd54ca324b1ac2c5cef2c45f1b712
Lina <lhabazil@gmail.com>
2018-12-29 00:44:58 -0600

    nav underline and transformation fix

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -11,15 +11,12 @@ import Contact from './Contact'
 import About from './About'
 
 class Home extends Component {
-  prevRoute = "homeLink";
+  prevRoute = "/";
   loaded = false;
   currRoute = "";
 
   componentDidMount() {
     console.log("component did mount");
-
-    // document.getElementById(this.prevRoute).style.borderBottomWidth = "3px";
-    //based on current url, transform screen to show content, i can scroll down when url is /about but it looks like home screen (basically adjust flex and tab indicator)
     
     var header = document.getElementById("header");
     var appContent = document.getElementById("appContent");
@@ -28,7 +25,7 @@ class Home extends Component {
     console.log(this.props);
     console.log(this.currRoute);
 
-    this.navigate(this.currRoute.substr(1) + 'Link');
+    this.navigate(this.currRoute);
   }
 
   navigate(path) {
@@ -39,7 +36,7 @@ class Home extends Component {
     var appContent = document.getElementById("appContent");
     console.log(header);
     console.log(appContent);
-    if (path == "homeLink") {
+    if (path == "/") {
       header.style.flex = 1;
       appContent.style.flex = 0;
       document.getElementById(this.prevRoute).style.borderBottomWidth = "0px";
@@ -48,60 +45,44 @@ class Home extends Component {
       header.style.flex = 0;
       appContent.style.flex = 1;
     }
-    if (path == "aboutLink") {
+    if (path == "/about") {
       document.getElementById(this.prevRoute).style.borderBottomWidth = "0px";
       document.getElementById(path).style.borderBottomWidth = "3px";
-    } else if (path == "portfolioLink") {
+    } else if (path == "/portfolio") {
       document.getElementById(this.prevRoute).style.borderBottomWidth = "0px";
       document.getElementById(path).style.borderBottomWidth = "3px";
-    } else if (path == "contactLink") {
+    } else if (path == "/contact") {
       document.getElementById(this.prevRoute).style.borderBottomWidth = "0px";
       document.getElementById(path).style.borderBottomWidth = "3px";
     }
     this.prevRoute = path;
   }
 
-  //try having loading spinner, after "header" is rendered, call navigate
-
   render() {
     console.log("start render");
     console.log(this.props);
-    // if (this.loaded) { 
-    //   var header = document.getElementById("header");
-    //   var appContent = document.getElementById("appContent");
-    //   console.log(header);
-    //   console.log(appContent);
-    //   console.log(this.props);
-    // }
+
     return (
       <Router>
         <div id="appContainer">
-
-          {/* <div id="header">
-            <Link to="/" id="headerLink" onClick={() => this.navigate("homeLink")}><h1>LINA HABAZI</h1></Link>
-            <h2>artist&nbsp;&nbsp;•&nbsp;&nbsp;developer&nbsp;&nbsp;•&nbsp;&nbsp;learner</h2>
-          </div> */}
-
           <Route render={(props) => {
             console.log(props.location);
             this.currRoute = props.location.pathname;
-            // navigate(props.location.pathname.substr(1))
-            // this.navigate(props.location.pathname.substr(1)+'Link');
             return (
               <div id="header">
-                <Link to="/" id="headerLink" onClick={() => this.navigate("homeLink")}><h1>LINA HABAZI</h1></Link>
+                <Link to="/" id="headerLink" onClick={() => this.navigate("/")}><h1>LINA HABAZI</h1></Link>
                 <h2>artist&nbsp;&nbsp;•&nbsp;&nbsp;developer&nbsp;&nbsp;•&nbsp;&nbsp;learner</h2>
               </div>
           )}} />
 
           <nav>
             <ul>
-              <li><Link to="/" class="navLink" id="homeLink" onClick={() => this.navigate("homeLink")}>HOME</Link></li>
-              <li><Link to="/about" class="navLink" id="aboutLink" onClick={() => this.navigate("aboutLink")}>ABOUT</Link></li>
-              <li><Link to="/portfolio" class="navLink" id="portfolioLink" onClick={() => this.navigate("portfolioLink")}>PORTFOLIO</Link></li>
-              <li><Link to="/contact" class="navLink" id="contactLink" onClick={() => this.navigate("contactLink")}>CONTACT</Link></li>
-              <li><a href="https://instagram.com/ni3lina" target="_blank" class="navLink" id="instaLink">INSTAGRAM</a></li>
-              <li><a href="https://github.com/linahabazi" target="_blank" class="navLink" id="githubLink">GITHUB</a></li>
+              <li><Link to="/" class="navLink" id="/" onClick={() => this.navigate("/")}>HOME</Link></li>
+              <li><Link to="/about" class="navLink" id="/about" onClick={() => this.navigate("/about")}>ABOUT</Link></li>
+              <li><Link to="/portfolio" class="navLink" id="/portfolio" onClick={() => this.navigate("/portfolio")}>PORTFOLIO</Link></li>
+              <li><Link to="/contact" class="navLink" id="/contact" onClick={() => this.navigate("/contact")}>CONTACT</Link></li>
+              <li><a href="https://instagram.com/ni3lina" target="_blank" class="navLink" id="insta">INSTAGRAM</a></li>
+              <li><a href="https://github.com/linahabazi" target="_blank" class="navLink" id="github">GITHUB</a></li>
             </ul>
           </nav>
 
